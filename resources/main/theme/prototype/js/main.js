38e55409a3bf3bdc9b75b31aa5c40870079df5ea
KYoung <kyoung.k.0613@gmail.com>
2022-07-25 17:25:13 +0900

    [2022. 7. 25. 오후 5:25:13] guts deployment commit.

---
+++ resources/main/theme/prototype/js/main.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+
+    //depth menu
+    $('.menu').click(function () {
+
+        var num = $(this).index();
+        $('.sub').eq(num).addClass('on').parents().siblings().children().removeClass('on');
+
+        $('.sub_bg').addClass('on');
+    });
+
+    //depth menu 외 클릭 시 효과 제거
+    $('html').click(function (e) {
+        if ($(e.target).parents('#wrap').length < 1) {
+            //alert('메뉴 외 부분이 맞습니다')
+            $('.sub_bg, .sub').removeClass('on');
+        }
+    });
+});
